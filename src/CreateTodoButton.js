3f57bfdcc16e75223729a6b6618934fcb7b0905a
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2023-02-05 17:59:39 -0600

    Events

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,18 @@ import React from './CreateTodoButton';
 import './CreateTodoButton.css';
 
 function CreateTodoButton() {
-    return(
-        <button className="CreateTodoButton">+</button>
+    const onClickButton = (msg) => {
+        alert(msg);
+    }
+
+
+    return (
+        <button 
+            className="CreateTodoButton"
+            onClick={() => onClickButton('Modal desplegado...')}
+        >
+            +
+        </button>
     );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value)
+    }
+
     return (
-        <input className="TodoSearch" placeholder='¿Qué hice?' />
+        <input 
+            className="TodoSearch" 
+            placeholder='¿Qué hice?'
+            onChange={onSearchValueChange} 
+        />
     );
 }
 
